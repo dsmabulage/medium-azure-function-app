764e102d4b3b960ffe6dbd1c19b0180e228b3570
Dileepa Mabulage <dileepainivos@gmail.com>
2025-03-09 13:58:24 +0530

    first commit

---
+++ FunctionApp1/Function1.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp1
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(Function1))]
+        public async Task Run([BlobTrigger("test/{name}", Connection = "CONN_STRING")] Stream stream, string name)
+        {
+           
+            _logger.LogInformation($"C# Blob trigger function Processed blob Name: {name}");
+        }
+    }
+}
